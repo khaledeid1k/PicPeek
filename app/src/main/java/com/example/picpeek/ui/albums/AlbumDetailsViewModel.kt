c7f292160b09e58ba172733146c50fa39d19a787
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-12-09 11:44:18 +0200

    apply search feature

--- app/src/main/java/com/example/picpeek/ui/albums/AlbumDetailsViewModel.kt
+++ app/src/main/java/com/example/picpeek/ui/albums/AlbumDetailsViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.picpeek.ui.albums
 
-import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -11,6 +10,9 @@ import com.example.picpeek.util.Constants.ALBUM_ID
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,24 +23,52 @@ class AlbumDetailsViewModel @Inject constructor(
 ) : ViewModel() {
     private val _albumsDetail = MutableStateFlow<ApiState<List<AlbumsDetails>>>(ApiState.Loading)
     val albumsDetail: StateFlow<ApiState<List<AlbumsDetails>>> = _albumsDetail
+    val searchQuery = MutableStateFlow("")
 
     fun saveAlbumId(userId: Int) {
         savedStateHandle[ALBUM_ID] = userId
     }
 
     private fun getAlbumId(): Int {
-        return savedStateHandle[ALBUM_ID] ?:0
+        return savedStateHandle[ALBUM_ID] ?: 0
     }
 
 
-     fun getAlbumsDetails() {
+    fun getAlbumsDetails() {
         viewModelScope.launch {
-            Log.d("TAG", "getAlbumsDetails:${getAlbumId()} ")
             repo.getAlbumsDetails(getAlbumId()).collect {
-                _albumsDetail.value = it
+                when (it) {
+                    is ApiState.Error -> {
+                        _albumsDetail.value = ApiState.Error(it.message)
+                    }
+
+                    is ApiState.Loading -> {
+                        _albumsDetail.value = ApiState.Loading
+                    }
+
+                    is ApiState.Success -> {
+                        searchQuery.collect {query->
+                            processSearchQuery(query,it.data)
+                        }
+
+                        }
+
+
+
+                    }
+                }
             }
         }
+
+
+    private suspend fun processSearchQuery(query: String, data: List<AlbumsDetails>?) {
+        val toList = data?.asFlow()
+            ?.filter { albums -> albums.title.contains(query) }
+            ?.toList()
+        _albumsDetail.value = ApiState.Success(toList)
     }
+
+
 
     fun retryToConnectNetWork() {
         getAlbumsDetails()
