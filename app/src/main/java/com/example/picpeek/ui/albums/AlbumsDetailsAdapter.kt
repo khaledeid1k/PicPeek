ced36af590a687557cc3c0ca0b9d20a15938408d
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-12-09 12:58:42 +0200

    add base adapter

--- app/src/main/java/com/example/picpeek/ui/albums/AlbumsDetailsAdapter.kt
+++ app/src/main/java/com/example/picpeek/ui/albums/AlbumsDetailsAdapter.kt
@@ -1,51 +1,14 @@
 package com.example.picpeek.ui.albums
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
 import com.example.picpeek.R
-import com.example.picpeek.databinding.ItemAlbumDetailsBinding
 import com.example.picpeek.domain.model.AlbumsDetails
-import com.example.picpeek.ui.base.BaseDataDiffUtil
+import com.example.picpeek.ui.base.BaseAdapter
+import com.example.picpeek.ui.base.BaseListener
 
-class AlbumsDetailsAdapter(private val albumsDetailsListener: AlbumsDetailsListener)  : RecyclerView.Adapter<AlbumsDetailsAdapter.AlbumsDetailsViewHolder>() {
-
-    private var albumsDetails: List<AlbumsDetails> = emptyList()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumsDetailsViewHolder {
-        return AlbumsDetailsViewHolder(
-            DataBindingUtil.inflate(
-                LayoutInflater.from(parent.context), R.layout.item_album_details,
-                parent, false)
-        )
-    }
-    override fun onBindViewHolder(holder: AlbumsDetailsViewHolder, position: Int) {
-        val albumsDetail = albumsDetails[position]
-        holder.bind(albumsDetail,albumsDetailsListener)
-    }
-    override fun getItemCount()=albumsDetails.size
-
-    class AlbumsDetailsViewHolder(private val binding: ItemAlbumDetailsBinding):
-        RecyclerView.ViewHolder(binding.root){
-            fun bind(albumsDetail: AlbumsDetails,albumsDetailsListener :AlbumsDetailsListener) {
-            binding.item = albumsDetail
-            binding.listener = albumsDetailsListener
-        }
-    }
-
-    fun setItems(newItems: List<AlbumsDetails>) {
-        val diffResult = DiffUtil.calculateDiff(
-            BaseDataDiffUtil(albumsDetails, newItems,
-                checkItemsTheSame=    { oldItemPosition, newItemPosition -> albumsDetails[oldItemPosition].title == newItems[newItemPosition].title },
-                checkContentsTheSame=  { oldItemPosition, newItemPosition -> albumsDetails[oldItemPosition] == newItems[newItemPosition] }
-            )
-        )
-        albumsDetails = newItems
-        diffResult.dispatchUpdatesTo(this)
-    }
-    interface AlbumsDetailsListener { fun onClickAlbumPhoto(albumsDetails: AlbumsDetails) }
+class AlbumsDetailsAdapter(albumsDetailsListener: AlbumsDetailsListener)  :
+    BaseAdapter<AlbumsDetails>(albumsDetailsListener) {
+    override val layoutIdFragment=R.layout.item_album_details
+    interface AlbumsDetailsListener:BaseListener { fun onClickAlbumPhoto(albumsDetails: AlbumsDetails) }
 
 
 
--- app/src/main/java/com/example/picpeek/ui/profile/AlbumsUserAdapter.kt
+++ app/src/main/java/com/example/picpeek/ui/profile/AlbumsUserAdapter.kt
@@ -1,52 +1,14 @@
 package com.example.picpeek.ui.profile
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
 import com.example.picpeek.R
-import com.example.picpeek.databinding.ItemUserAlbumBinding
 import com.example.picpeek.domain.model.UserAlbums
-import com.example.picpeek.ui.base.BaseDataDiffUtil
+import com.example.picpeek.ui.base.BaseAdapter
+import com.example.picpeek.ui.base.BaseListener
 
-class AlbumsUserAdapter(private val albumsUserListener: AlbumsUserListener)  : RecyclerView.Adapter<AlbumsUserAdapter.AlbumsUserViewHolder>() {
-
-    private var userAlbums: List<UserAlbums> = emptyList()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumsUserViewHolder {
-        return AlbumsUserViewHolder(
-            DataBindingUtil.inflate(
-                LayoutInflater.from(parent.context), R.layout.item_user_album,
-                parent, false)
-        )
-    }
-    override fun onBindViewHolder(holder: AlbumsUserViewHolder, position: Int) {
-        val userAlbum = userAlbums[position]
-        holder.bind(userAlbum,albumsUserListener)
-    }
-    override fun getItemCount()=userAlbums.size
-
-    class AlbumsUserViewHolder (private val binding: ItemUserAlbumBinding):
-        RecyclerView.ViewHolder(binding.root){
-            fun bind(userAlbum: UserAlbums,albumsUserListener :AlbumsUserListener) {
-            binding.item = userAlbum
-            binding.listener = albumsUserListener
-        }
-    }
-
-    fun setItems(newItems: List<UserAlbums>) {
-        val diffResult = DiffUtil.calculateDiff(
-            BaseDataDiffUtil(userAlbums, newItems,
-                checkItemsTheSame=    { oldItemPosition, newItemPosition -> userAlbums[oldItemPosition].title == newItems[newItemPosition].title },
-                checkContentsTheSame=  { oldItemPosition, newItemPosition -> userAlbums[oldItemPosition] == newItems[newItemPosition] }
-            )
-        )
-        userAlbums = newItems
-        diffResult.dispatchUpdatesTo(this)
-    }
-    interface AlbumsUserListener { fun onClickAlbum(userAlbum: UserAlbums) }
-
+class AlbumsUserAdapter( albumsUserListener: AlbumsUserListener):
+BaseAdapter<UserAlbums>(albumsUserListener) {
+    override val layoutIdFragment=R.layout.item_user_album
+    interface AlbumsUserListener:BaseListener { fun onClickAlbum(userAlbum: UserAlbums) }
 
 
 }
