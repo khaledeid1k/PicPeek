e9e674e23496c984f8661542febb50763ed99ef0
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-12-09 09:56:06 +0200

    make user albums parcelable

--- app/src/main/java/com/example/picpeek/domain/model/UserAlbums.kt
+++ app/src/main/java/com/example/picpeek/domain/model/UserAlbums.kt
@@ -1,6 +1,10 @@
 package com.example.picpeek.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class UserAlbums(
     val id: Int,
     val title: String
-)
+) : Parcelable
